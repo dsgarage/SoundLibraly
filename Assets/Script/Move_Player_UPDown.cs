e387ac352e4fe3abc9bf5ceb8949fe7c0a58ca7e
Daisuke Tsukada <dsgarage@gmail.com>
2013-09-25 12:32:51 +0900

    フォルダ整理、コードリファレンス追加

---
+++ Assets/Script/Move_Player_UPDown.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//[RequireComponent(typeof(Rigidbody))]
+public class Move_Player_UPDown : MonoBehaviour 
+{
+
+	public float speed = 6.0f;
+	public enum MovePattern{
+	UPDownMove, UPDownStop
+	}
+
+	void Awake()
+	{
+//	gameObject.AddComponent<Rigidbody>();		
+	}
+
+	public void Update()
+	{
+
+		// 上下に対応する入力による上下移動 要Rigidbody
+//		rigidbody.velocity = new Vector3(0, Input.GetAxisRaw("Vertical") * speed, 0);
+
+
+		transform.position += new Vector3(0, Input.GetAxisRaw("Vertical") * speed * Time.deltaTime, 0);
+
+	}
+
+
+}
+ 
