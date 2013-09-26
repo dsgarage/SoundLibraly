65d22736ec63a1f49fad39425963e488f7d9188d
Daisuke Tsukada <dsgarage@gmail.com>
2013-09-26 18:05:15 +0900

    Script：DebugDrawRay.cs、RigidbodyAddForce Shader：HorizontalSkybox.shader、GradientSkybox.shader 追加

---
+++ Assets/Script/DebugDrawRay.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugDrawRay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 up = transform.TransformDirection(Vector3.up) * 10;
+        Debug.DrawRay(transform.position, up, Color.green);
+	}
+
+	private void OnCollisionEnter(Collision collision)
+	{
+//		Destroy(collision.gameObject);
+        foreach (ContactPoint contact in collision.contacts) {
+            Debug.DrawRay(contact.point, contact.normal, Color.red, 2.0f);
+        }
+     
+	}
+
+}
---
+++ Assets/Script/Rigidbody_AddFoece.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rigidbody_AddFoece : MonoBehaviour {
+
+public GameObject bigCube;
+public Vector3 bigCubeStartPos;
+public Quaternion bigCubeStartRot;
+
+public GameObject smallCube;
+public Vector3 smallCubeStartPos;
+public Quaternion smallCubeStartRot;
+
+
+	// Use this for initialization
+	void Start () {
+
+	bigCubeStartPos = bigCube.transform.position;
+	smallCubeStartPos = smallCube.transform.position;
+
+	bigCubeStartRot = bigCube.transform.rotation;
+	smallCubeStartRot = smallCube.transform.rotation;
+
+	bigCube.rigidbody.Sleep();
+	smallCube.rigidbody.Sleep();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+	if (GUILayout.Button("Reset")) {
+		bigCube.rigidbody.Sleep();
+		smallCube.rigidbody.Sleep();
+		bigCube.transform.position = bigCubeStartPos;
+		smallCube.transform.position = smallCubeStartPos;
+		bigCube.transform.rotation = bigCubeStartRot;
+		smallCube.transform.rotation = smallCubeStartRot;
+	}
+	
+	Vector3 dir = Vector3.right;
+
+	if (GUILayout.Button("Force")) {
+		bigCube.rigidbody.AddForce(dir.normalized * 200f, ForceMode.Force);
+		smallCube.rigidbody.AddForce(dir.normalized * 200f, ForceMode.Force);
+	}
+
+	if (GUILayout.Button("Acceleration")) {
+		bigCube.rigidbody.AddForce(dir.normalized * 200f, ForceMode.Acceleration);
+		smallCube.rigidbody.AddForce(dir.normalized * 200f, ForceMode.Acceleration);
+	}
+	if (GUILayout.Button("Impulse")) {
+		bigCube.rigidbody.AddForce(dir.normalized * 10f, ForceMode.Impulse);
+		smallCube.rigidbody.AddForce(dir.normalized * 10f, ForceMode.Impulse);
+	}
+	if (GUILayout.Button("VelocityChange")) {
+		bigCube.rigidbody.AddForce(dir.normalized * 10f, ForceMode.VelocityChange);
+		smallCube.rigidbody.AddForce(dir.normalized * 10f, ForceMode.VelocityChange);
+	}
+	}
+
+}
